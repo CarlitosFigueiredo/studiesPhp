158eee3de9937ea92e36c8b3e248b6a1316528e7
Carlos Figueiredo <carlos.yuri@itcnet.com.br>
2025-07-23 09:16:30 -0300

    Conditionals and Booleans

--- demo/index.php
+++ demo/index.php
@@ -5,15 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>
-        <?php
-            $greeting = "Hello";
+    <?php
+        $name = "Dark Matter";
+        $read = false;
 
-            echo "$greeting Everybody!";
-        ?>
+        if ($read) {
+
+            $message = "You have read $name";
+        } else {
+
+            $message = "You have Not read $name";
+        }
+    ?>
+
+    <h1>
+        <?= $message; ?>
     </h1>
 </body>
 

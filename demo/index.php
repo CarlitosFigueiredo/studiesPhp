48a7b0a9d3ce2fde7abe846442aa2ffc1e47d93b
Carlos Figueiredo <carlos.yuri@itcnet.com.br>
2025-07-23 08:53:09 -0300

    Variables

--- demo/index.php
+++ demo/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Demo</title>
 </head>
+
 <body>
     <h1>
-        <?php  
-            echo "Hello, Word";
+        <?php
+            $greeting = "Hello";
+
+            echo "$greeting Everybody!";
         ?>
     </h1>
 </body>
+
 </html>

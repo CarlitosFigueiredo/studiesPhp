97de5f12a803183f6cef36d654924b3807fb9b54
Carlos Yuri Figueiredo <92981178+CarlitosFigueiredo@users.noreply.github.com>
2025-07-26 16:20:04 -0300

    Separate Logic From the Template

--- demo/index.php
+++ demo/index.php
@@ -1,68 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo</title>
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com',
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com',
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
 
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100vh;
-            margin: 0;
-            font-family: sans-serif;
-        }
-    </style>
-</head>
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['author'] === 'Andy Weir';
+});
 
-<body>
-    <?php
-    $books = [
-        [
-            'name' => 'Do Androids Dream of Electric Sheep',
-            'author' => 'Philip K. Dick',
-            'releaseYear' => 1968,
-            'purchaseUrl' => 'http://example.com',
-        ],
-        [
-            'name' => 'Project Hail Mary',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2021,
-            'purchaseUrl' => 'http://example.com',
-        ],
-        [
-            'name' => 'The Martian',
-            'author' => 'Andy Weir',
-            'releaseYear' => 2011,
-            'purchaseUrl' => 'http://example.com'
-        ],
-    ];
-
-    $filteredBooks = array_filter($books, function ($book) {
-        return $book['author'] === 'Andy Weir';
-    });
-    ?>
-
-    <h1>
-        Recommended Books
-    </h1>
-
-    <ul>
-        <?php foreach ($filteredBooks as $book) : ?>
-            <li>
-                <a href=" <?= $book['purchaseUrl'] ?> ">
-
-                    <?= $book['name'] ?> <br />
-
-                    (<?= $book['releaseYear'] ?>)
-
-                    <hr />
-                </a>
-            </li>
-        <?php endforeach ?>
-    </ul>
-</body>
-
-</html>
+require 'index.view.php';
---
+++ demo/index.view.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+
+
+    <h1>
+        Recommended Books
+    </h1>
+
+    <ul>
+        <?php foreach ($filteredBooks as $book) : ?>
+            <li>
+                <a href=" <?= $book['purchaseUrl'] ?> ">
+
+                    <?= $book['name'] ?> <br />
+
+                    (<?= $book['releaseYear'] ?>)
+
+                    <hr />
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+</body>
+
+</html>

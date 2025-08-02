2c89ea2ce813445e63c267bd602b1876c3fb3690
Carlos Yuri Figueiredo <92981178+CarlitosFigueiredo@users.noreply.github.com>
2025-08-02 16:54:26 -0300

    Render the Notes and Note page

--- demo/index.php
+++ demo/index.php
@@ -1,17 +1,8 @@
 <?php
 
-require 'Database.php';
 require 'functions.php';
-// require 'router.php';
+require 'Database.php';
+require 'router.php';
 
-$config = require('config.php');
-
-$db = new Database($config['database'], 'root', 'Yy-96746766');
-
-$id = $_GET['id'];
-
-$query = "select * from posts where id = :id";
-
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
+// create table notes / id / body (text) / not null
+// create table users/ id / name (varchar 255)/ email (varchar 255) unique / user_id (int) not null / foreign_key
--- demo/views/partials/nav.php
+++ demo/views/partials/nav.php
@@ -7,32 +7,37 @@
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                        <a 
-                            href="/" 
+                        <a
+                            href="/"
                             class="
                                 <?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> 
                                 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium
-                            " 
-                            aria-current="page"
-                        >
+                            "
+                            aria-current="page">
                             Home
                         </a>
-                        <a 
-                            href="/about" 
+                        <a
+                            href="/about"
                             class="
                                 <?= urlIs('/about') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> 
                                 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium
-                            "     
-                        >
+                            ">
                             About
                         </a>
-                        <a 
-                            href="/contact" 
+                        <a
+                            href="/notes"
+                            class="
+                                <?= urlIs('/notes') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> 
+                                hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium
+                            ">
+                            Notes
+                        </a>
+                        <a
+                            href="/contact"
                             class="
                                 <?= urlIs('/contact') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> 
                                 hover:bg-gray-700 px-3 py-2 rounded-md text-sm font-medium
-                            "
-                        >
+                            ">
                             Contact
                         </a>
                     </div>

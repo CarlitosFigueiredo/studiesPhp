446459a11313b01ebee9a7de64a689c21021ebac
Carlos Figueiredo <carlos.yuri@itcnet.com.br>
2025-08-07 09:03:51 -0300

    Extract a Simple Validator Class

--- demo/controllers/note-create.php
+++ demo/controllers/note-create.php
@@ -1,6 +1,9 @@
 <?php
 
-$config = require('config.php');
+require 'Validator.php';
+
+$config = require 'config.php';
+
 $db = new Database($config['database'], 'root', 'Yy-96746766');
 
 $heading = 'Create Note';
@@ -9,13 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $errors = [];
 
-    if (strlen($_POST['body']) === 0 ) {
-        $errors['body'] = 'A body is required';
-    }
+    if (! Validator::string($_POST['body'], 1, 1000)) {
 
-    if (strlen($_POST['body']) > 200 ) {
-        $errors['body'] = 'The body can not be more than 200 characters.';
-    } 
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
+    }
 
     if (empty($errors)) {
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [

e5b0b8977e6da98791efbd12472234512c78d6cb
Carlos Yuri Figueiredo <92981178+CarlitosFigueiredo@users.noreply.github.com>
2025-08-10 10:33:22 -0300

    Build a Better Router

--- demo/views/notes/show.view.php
+++ demo/views/notes/show.view.php
@@ -17,6 +17,11 @@
         <form class="mt-6" method="POST">
             <input
                 type="hidden"
+                name="_method"
+                value="DELETE" />
+
+            <input
+                type="hidden"
                 name="id"
                 value="<?= $note['id'] ?>" />
 

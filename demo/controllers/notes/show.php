703f38b700880fdcd9ae64f661c58109c932421a
Carlos Figueiredo <carlos.yuri@itcnet.com.br>
2025-08-11 11:15:22 -0300

    Make Your First Service Container - Part One

--- demo/controllers/notes/show.php
+++ demo/controllers/notes/show.php
@@ -1,11 +1,11 @@
 <?php
 
+use Core\App;
 use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database'], 'root', '96746766');
+$db = App::resolve(Database::class);
 
-$currentUserId = '1';
+$currentUserId = 1;
 
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
